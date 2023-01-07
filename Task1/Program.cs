ac263b45b36d8cb0912bde1516e8fa5632051231
Aleksey <a.y.zayitsev@gmail.com>
2023-01-08 01:11:18 +0300

    Программа для определение большего числа

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write ("Ввеси первое число: ");
+int numA = Convert.ToInt32 (Console.ReadLine ());
+Console.Write ("Ввести второе число: ");
+int numB = Convert.ToInt32 (Console.ReadLine ());
+
+if (numA > numB)
+{
+    Console.Write ("Большим из этих чисел является - ");
+    Console.WriteLine (numA);
+}
+else if (numA == numB)
+{
+    Console.Write ("Эти числа равны");
+}
+else
+{
+    Console.Write ("Большим из этих чисел является - ");
+    Console.WriteLine (numB);
+}
