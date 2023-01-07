07867cc0ac2a946b28ce96f4eef59e8c8c26330b
Aleksey <a.y.zayitsev@gmail.com>
2023-01-08 01:25:20 +0300

    программа для вычесления большего числа из трех

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write ("Введите первое число: ");
+int numA = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int numB = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Введите третье число: ");
+int numC = Convert.ToInt32 (Console.ReadLine());
+
+int max = numA;
+
+if (numA > max) max = numA;
+if (numB > max) max =numB;
+if (numC > max) max =numC;
+
+Console.Write ("Самым большим числом является - ");
+Console.WriteLine (max);
